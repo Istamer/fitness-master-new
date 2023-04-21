3e5c352f647aace376ac1b6f2c4db5d38af69ded
Ilona <catandmoonstone@gmail.com>
2023-04-21 20:21:59 +0300

    add screenshot

--- client/src/components/Letters.jsx
+++ client/src/components/Letters.jsx
@@ -12,32 +12,17 @@ const Letters = () => {
         navigator(appRoutes.profilePurchase.path);
     };
 
-    const handleBoughtClick = () => {
-        navigator(appRoutes.profile.path);
-    };
-
     return(
         <div className="w-full py-16 text-white px-4">
             <div className="max-w-[1240px] mx-auto grid lg:grid-cols-3">
                 <div className="lg:col-span-2 my-3">
-                    {user.isPremium ?
-                        <h1 className="md:text-3xl sm:text-3xl text-xl font-bold">Дякуємо, що придбали особистий профіль! Ваша підтримка є для нас важливою.</h1> :
-                        <h1 className="md:text-3xl sm:text-3xl text-xl font-bold">Отримай можливість стежити за своїм прогресом разом з власним профілем!</h1>
-                    }
+                    <h1 className="md:text-3xl sm:text-3xl text-xl font-bold">Отримай можливість стежити за своїм прогресом разом з власним профілем!</h1>
                     <p className="font-light text-gray-400">Придбання є одноразовим на весь період користування.</p>
                 </div>
                <div> <div className="flex flex-col sm:flex-row items-center justify-between w-full">
-
-                   {user.isPremium ?
-                       <button onClick={handleBoughtClick} className="bg-[#FF7F50] w-[500px] rounded-md font-bold text-black ml-3 my-6 mx-auto py-3">Перейти до профілю</button> :
-                       <button onClick={handleBuyClick} className="bg-[#FF7F50] w-[500px] rounded-md font-bold text-black ml-3 my-6 mx-auto py-3">Переглянути інформацію</button>
-                   }
+                    <button onClick={handleBuyClick} className="bg-[#FF7F50] w-[500px] rounded-md font-bold text-black ml-3 my-6 mx-auto py-3">Переглянути інформацію</button>
                 </div>
-                   {user.isPremium ?
-                       <p className="font-light text-gray-400 ">Ви вже придбали профіль.</p> :
-                       <p className="font-light text-gray-400 ">Перегляньте умови оплати та опис можливостей.</p>
-                   }
-                   </div>
+                <p className="font-light text-gray-400 ">Перегляньте умови оплати та опис можливостей.</p></div>
             </div>
         </div>
     )
